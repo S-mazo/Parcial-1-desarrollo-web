381f6d6e8963a34feb2198e9e5f61315e464f9a7
Firstonefire <213894236+S-mazo@users.noreply.github.com>
2025-09-14 17:48:05 -0500

    Update routes
    Now routes are the same on readme and routes

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ let userController = require('../controllers/users');
 let router = express.Router();
 
 
-router.post('/api/user', userController.createUser);
-router.post('/api/login', userController.loginUser);
+router.post('/api/users/register', userController.createUser);
+router.post('/api/users/login', userController.loginUser);
 
 
 module.exports = router;
